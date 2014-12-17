a2b60beb1c0e9c17068b2fa46e937acef7b9311b
Marvin Humphrey <marvin@rectangular.com>
2014-12-16 16:37:54 -0800

    wip compiler tests

---
+++ compiler/python/test/test_cfc.py
@@ -0,0 +1,11 @@
+import unittest
+
+class MyTest(unittest.TestCase):
+
+    def testMethod(self):
+        self.assertTrue(True, "True should be true")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
