129faf4410514423f24189a10eed1a35362841d7
Marvin Humphrey <marvin@rectangular.com>
2015-02-17 17:39:37 -0800

    Make CFCPyClass instantiable.

--- compiler/src/CFCPyClass.c
+++ compiler/src/CFCPyClass.c
@@ -20,11 +20,112 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define CFC_NEED_BASE_STRUCT_DEF 1
+
+#include "CFCBase.h"
 #include "CFCPyClass.h"
+#include "CFCParcel.h"
 #include "CFCUtil.h"
 #include "CFCClass.h"
 #include "CFCFunction.h"
 #include "CFCPyMethod.h"
+
+struct CFCPyClass {
+    CFCBase base;
+    CFCParcel *parcel;
+    char *class_name;
+    CFCClass *client;
+};
+
+static CFCPyClass **registry = NULL;
+static size_t registry_size = 0;
+static size_t registry_cap  = 0;
+
+static const CFCMeta CFCPERLCLASS_META = {
+    "Clownfish::CFC::Binding::Perl::Class",
+    sizeof(CFCPyClass),
+    (CFCBase_destroy_t)CFCPyClass_destroy
+};
+
+CFCPyClass*
+CFCPyClass_new(CFCParcel *parcel, const char *class_name) {
+    CFCUTIL_NULL_CHECK(parcel);
+    CFCUTIL_NULL_CHECK(class_name);
+    CFCPyClass *self = (CFCPyClass*)CFCBase_allocate(&CFCPERLCLASS_META);
+    self->parcel = (CFCParcel*)CFCBase_incref((CFCBase*)parcel);
+    self->class_name = CFCUtil_strdup(class_name);
+    // Client may be NULL, since fetch_singleton() does not always succeed.
+    CFCClass *client = CFCClass_fetch_singleton(parcel, class_name);
+    self->client = (CFCClass*)CFCBase_incref((CFCBase*)client);
+    return self;
+}
+
+void
+CFCPyClass_destroy(CFCPyClass *self) {
+    CFCBase_decref((CFCBase*)self->parcel);
+    CFCBase_decref((CFCBase*)self->client);
+    FREEMEM(self->class_name);
+    CFCBase_destroy((CFCBase*)self);
+}
+
+static int
+S_compare_cfcperlclass(const void *va, const void *vb) {
+    CFCPyClass *a = *(CFCPyClass**)va;
+    CFCPyClass *b = *(CFCPyClass**)vb;
+    return strcmp(a->class_name, b->class_name);
+}
+
+void
+CFCPyClass_add_to_registry(CFCPyClass *self) {
+    if (registry_size == registry_cap) {
+        size_t new_cap = registry_cap + 10;
+        registry = (CFCPyClass**)REALLOCATE(registry,
+                                            (new_cap + 1) * sizeof(CFCPyClass*));
+        for (size_t i = registry_cap; i <= new_cap; i++) {
+            registry[i] = NULL;
+        }
+        registry_cap = new_cap;
+    }
+    CFCPyClass *existing = CFCPyClass_singleton(self->class_name);
+    if (existing) {
+        CFCUtil_die("Class '%s' already registered", self->class_name);
+    }
+    registry[registry_size] = (CFCPyClass*)CFCBase_incref((CFCBase*)self);
+    registry_size++;
+    qsort(registry, registry_size, sizeof(CFCPyClass*),
+          S_compare_cfcperlclass);
+}
+
+CFCPyClass*
+CFCPyClass_singleton(const char *class_name) {
+    CFCUTIL_NULL_CHECK(class_name);
+    for (size_t i = 0; i < registry_size; i++) {
+        CFCPyClass *existing = registry[i];
+        if (strcmp(class_name, existing->class_name) == 0) {
+            return existing;
+        }
+    }
+    return NULL;
+}
+
+CFCPyClass**
+CFCPyClass_registry() {
+    if (!registry) {
+        registry = (CFCPyClass**)CALLOCATE(1, sizeof(CFCPyClass*));
+    }
+    return registry;
+}
+
+void
+CFCPyClass_clear_registry(void) {
+    for (size_t i = 0; i < registry_size; i++) {
+        CFCBase_decref((CFCBase*)registry[i]);
+    }
+    FREEMEM(registry);
+    registry_size = 0;
+    registry_cap  = 0;
+    registry      = NULL;
+}
 
 char*
 CFCPyClass_pytype_struct_def(CFCClass *klass, const char *pymod_name) {
--- compiler/src/CFCPyClass.h
+++ compiler/src/CFCPyClass.h
@@ -21,8 +21,48 @@
 extern "C" {
 #endif
 
+typedef struct CFCPyClass CFCPyClass;
 struct CFCParcel;
 struct CFCClass;
+
+/** Clownfish::CFC::Binding::Python::Class - Generate Python binding code for a
+ * Clownfish::CFC::Model::Class.
+ */
+
+/**
+ * @param parcel A Clownfish::CFC::Model::Parcel.
+ * @param class_name The name of the class to be registered.
+ */
+CFCPyClass*
+CFCPyClass_new(struct CFCParcel *parcel, const char *class_name);
+
+CFCPyClass*
+CFCPyClass_init(CFCPyClass *self, struct CFCParcel *parcel,
+                const char *class_name);
+
+void
+CFCPyClass_destroy(CFCPyClass *self);
+
+/** Add a new class binding to the registry.  Each unique parcel/class-name
+ * combination may only be registered once.
+ */
+void
+CFCPyClass_add_to_registry(CFCPyClass *self);
+
+/** Given a class name, return a class binding if one exists.
+ */
+CFCPyClass*
+CFCPyClass_singleton(const char *class_name);
+
+/** All registered class bindings.
+ */
+CFCPyClass**
+CFCPyClass_registry();
+
+/** Release all memory and references held by the registry.
+ */
+void
+CFCPyClass_clear_registry(void);
 
 char*
 CFCPyClass_pytype_struct_def(struct CFCClass *klass, const char *pymod_name);
