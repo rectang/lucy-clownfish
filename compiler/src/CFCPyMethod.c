15d911f64a4ed96f91b6d13ec2f68e2514924a8d
Marvin Humphrey <marvin@rectangular.com>
2016-02-02 13:42:27 -0800

    Gen list of converter/arg-pointer pairs.
    Generate the list of function-pointer/arg-pointer pairs passed which get
    passed to `PyArg_ParseTupleAndKeywords`.

--- compiler/src/CFCPyMethod.c
+++ compiler/src/CFCPyMethod.c
@@ -118,6 +118,53 @@ S_gen_declaration(CFCVariable *var, const char *val, int tick) {
     return result;
 }
 
+static char*
+S_gen_target(CFCVariable *var, const char *value, int tick) {
+    CFCType *type = CFCVariable_get_type(var);
+    const char *specifier = CFCType_get_specifier(type);
+    const char *micro_sym = CFCVariable_get_name(var);
+    const char *maybe_maybe = "";
+    const char *dest_name;
+    char *var_name = NULL;
+    if (CFCType_is_primitive(type)) {
+        dest_name = CFCType_get_specifier(type);
+        if (value != NULL) {
+            maybe_maybe = "maybe_";
+        }
+        var_name = CFCUtil_sprintf("cfargs[%d].%s_", tick, dest_name);
+    }
+    else if (CFCType_is_object(type)) {
+        if (CFCType_nullable(type) ||
+            (value && strcmp(value, "NULL") == 0)
+           ) {
+            maybe_maybe = "maybe_";
+        }
+        if (strcmp(specifier, "cfish_String") == 0) {
+            dest_name = "string";
+            var_name = CFCUtil_sprintf("cfargs[%d].ptr", tick);
+        }
+        else if (strcmp(specifier, "cfish_Hash") == 0) {
+            dest_name = "hash";
+            var_name = CFCUtil_sprintf("cfargs[%d].ptr", tick);
+        }
+        else if (strcmp(specifier, "cfish_Vector") == 0) {
+            dest_name = "vec";
+            var_name = CFCUtil_sprintf("cfargs[%d].ptr", tick);
+        }
+        else {
+            dest_name = "obj";
+            var_name = CFCUtil_sprintf("wrap_arg_%s", micro_sym);
+        }
+    }
+    else {
+        dest_name = "INVALID";
+    }
+    char *content = CFCUtil_sprintf(", CFBind_%sconvert_%s, &%s",
+                                    maybe_maybe, dest_name, var_name);
+    FREEMEM(var_name);
+    return content;
+}
+
 /* Generate the code which parses arguments passed from Python and converts
  * them to Clownfish-flavored C values.
  */
@@ -154,6 +201,10 @@ S_gen_arg_parsing(CFCParamList *param_list, int first_tick, char **error) {
         char *declaration = S_gen_declaration(var, val, i);
         declarations = CFCUtil_cat(declarations, declaration, NULL);
         FREEMEM(declaration);
+
+        char *target = S_gen_target(var, val, i);
+        targets = CFCUtil_cat(targets, target, NULL);
+        FREEMEM(target);
     }
 
     char parse_pattern[] =
